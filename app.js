35e2c459bc785f082a843083aef586aa626e8a1b
Alessandro <57524717+cappelloalessandro@users.noreply.github.com>
2021-07-15 00:13:39 +0200

    Abbiamo risolto cose
    incredibile, funziona il centramento

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ TweenMax.defaultEase = Linear.easeOut;
 
 new fullpage("#fullpage", {
   //options 
-  responsiveWidth: 800,
+  // responsiveWidth: 800,
   menu: '#menu',
   verticalCentered:false, //per mettere il flex
   autoScrolling: true,
